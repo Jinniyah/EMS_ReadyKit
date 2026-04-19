96bf434aaf16f53a3ad89955938f98e1b9e54443
Jinniyah <jinniyah@gmail.com>
2026-04-19 07:43:34 -0400

    Updating Terraform.

--- iac/Terraform/main.tf
+++ iac/Terraform/main.tf
@@ -1,2 +1,20 @@
 // Root module wiring child modules
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "ems_rg" {
+  name     = "rg-ems-readykit"
+  location = "eastus"
+}
