8a2d5198758977c587ccd8f62965e78f530d2075
Jinniyah <jinniyah@gmail.com>
2026-05-01 09:48:01 -0400

    Networking update and updating the gitignore file.

---
+++ iac/Terraform/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  subscription_id = "75fce2ea-1d83-4c5a-9929-b424b2913c8e"
+}
--- iac/Terraform/main.tf
+++ iac/Terraform/main.tf
@@ -1,2 +1,27 @@
 // Root module wiring child modules
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "ems_rg" {
+  name     = "rg-ems-readykit"
+  location = "eastus"
+}
+
+module "network" {
+  source = "./modules/network"
+
+  resource_group_name = azurerm_resource_group.ems_rg.name
+  location            = azurerm_resource_group.ems_rg.location
+}
